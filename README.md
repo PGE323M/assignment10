1649b9d64b24d26276f1be6ffaf46b4cac11b903
John T. Foster <johntfosterjr@gmail.com>
2020-10-06 17:20:46 -0500

    Update for 2020 and github actions

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # Homework Assignment 10
 
-[![Build Status](https://travis-ci.com/PGE323M/assignment10.svg?token=SnMGq692xXXqxzyE6QSj&branch=master)](https://travis-ci.com/PGE323M/assignment10)
+![Assignment 10](https://github.com/PGE323M/assignment10/workflows/.github/workflows/assignment10.yml/badge.svg)
+
 
 Open the [Assignment 10 Jupyter Notebook](assignment10.ipynb) and follow the instructions.
 
 ## Testing
 
-If you would like to check to see if your solution is correct, run the following commands at the Terminal command line in sequence:
-
-```bash
->jupyter nbconvert --to python assignment10.ipynb
-```
+If you would like to check to see if your solution is correct, run the following command at the Terminal command line:
 
 ```bash
 >python test.py
--- test.py
+++ test.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
 
-import numpy as np
+# Copyright 2018-2020 John T. Foster
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import unittest
+import nbconvert
+import numpy as np
+
+with open("assignment10.ipynb") as f:
+    exporter = nbconvert.PythonExporter()
+    python_file, _ = exporter.from_file(f)
+
+
+with open("assignment10.py", "w") as f:
+    f.write(python_file)
+
 
 from assignment10 import *
 
@@ -36,9 +59,10 @@ class TestSolution(unittest.TestCase):
                     'value': 0 #ft^3/day
                 }
             },
-            'numerical': {
+            'numerical': { 
                 'solver': 'implicit',
-                'number of grids': 4,
+                'number of grids': { 
+                 'x': 4},
                 'time step': 1, #day
                 'number of time steps' : 3 
             },
